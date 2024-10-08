cbbb2b8316c35cae42a7b6fa8b091ef411d4b6ee
Orla Clarke <Clarke-O11@ulster.ac.uk>
2024-10-08 15:07:39 +0100

    Interfaces

---
+++ Assets/CRE340/Game3-CodeCommunication/Scripts/Enemy.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour, IDamagable
+{
+
+    public int health = 10;
+    private Material mat;
+    private Color originalColour;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+        originalColour = mat.color;
+    }
+
+    public void TakeDamage(int damage) 
+    { 
+        health -= damage;
+        ShowHitEffect();
+
+        if (health <= 0) 
+        {
+            Die();
+        }
+    }
+
+    public void ShowHitEffect() 
+    { 
+        mat.color = Color.red;
+        Invoke("ResetMaterial", 0.1f);
+    }
+
+    private void ResetMaterial() 
+    { 
+        mat.color = originalColour;
+    }
+
+    private void Die() 
+    { 
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/CRE340/Game3-CodeCommunication/Scripts/ExplodingCrate.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodingCrate : MonoBehaviour, IDamagable
+{
+    public int health = 10;
+    public GameObject explosionEffectPrefab;
+    private Material mat;
+    private Color originalColour;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+        originalColour = mat.color;
+    }
+
+    public void TakeDamage(int damage) 
+    { 
+        health -= damage;
+        ShowHitEffect();
+
+        if (health <= 0) 
+        {
+            Explode();
+            Destroy(gameObject);
+        }  
+    }
+
+    public void ShowHitEffect() 
+    { 
+        mat.color = Color.red;
+        Invoke("ResetMaterial", 0.1f);
+    }
+
+    private void ResetMaterial() 
+    { 
+        mat.color = originalColour;
+    }
+
+    private void Explode() 
+    {
+        if (explosionEffectPrefab != null) 
+        { 
+            Instantiate(explosionEffectPrefab, transform.position, Quaternion.identity);
+        }
+    }
+
+}
